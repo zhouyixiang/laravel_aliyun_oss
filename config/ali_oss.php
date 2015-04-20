193133ee6c1d636ba798589dc5f2211d953c9df2
carl <zhouyxamp@gmail.com>
2015-04-20 18:37:37 +0800

    update configuration file to make use of laravel .env configuration

--- config/ali_oss.php
+++ config/ali_oss.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    'access_key' => '',
-    'access_id' => '',
-    'hostname' => ''
+    'access_id' => env('OSS_ACCESS_ID', ''),
+    'access_key' => env('OSS_ACCESS_KEY', ''),
+    'hostname' => env('OSS_HOSTNAME', ''),
+    'bucket' => env('OSS_BUCKET_NAME', '')
 ];
